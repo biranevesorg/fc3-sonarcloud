e481193b3789f4252b0e0e351f304463e2752c3a
Bira Neves <biraneves@outlook.com>
2025-02-07 08:04:08 -0300

    ci: :rocket: check if quality gate is working on sonar cloud

--- sum.go
+++ sum.go
@@ -4,21 +4,21 @@ import "fmt"
 
 func main() {
 	fmt.Println(soma(2, 2))
-	fmt.Println(subtracao(2, 2))
+	// fmt.Println(subtracao(2, 2))
 }
 
 func soma(a int, b int) int {
 	return a + b
 }
 
-func subtracao(a int, b int) int {
-	return a - b
-}
+// func subtracao(a int, b int) int {
+// 	return a - b
+// }
 
-func multiplicacao(a int, b int) int {
-	return a * b
-}
+// func multiplicacao(a int, b int) int {
+// 	return a * b
+// }
 
-func divisao(a int, b int) int {
-	return a / b
-}
+// func divisao(a int, b int) int {
+// 	return a / b
+// }
